32db57fd4bc4542aa822c42949913fe513da7da7
Angel Becerra <becerra.o.angel@gmail.com>
2026-09-24 15:42:18 -0700

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,15 @@
+integer_type = 1
+float_type = 1.0
+bool_type = False
+string_type = "hex"
+list_type = [23, 67, 99]
+
+
+
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
