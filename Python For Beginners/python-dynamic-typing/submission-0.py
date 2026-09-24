7ee612811ec5b58c368f61cbc0bb914b54f18323
Angel Becerra <becerra.o.angel@gmail.com>
2026-09-24 15:46:29 -0700

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,12 @@
+
+
+variable = 23
+print(type(variable))
+variable = 23.98
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "whats up bro"
+print(type(variable))
+variable = [1,2,3,4,4,56,6,77]
+print(type(variable))
